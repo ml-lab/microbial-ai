c675a60d0a904280d71a63ed7821122cb3286dd2
Dileep Kishore <k.dileep1994@gmail.com>
2017-10-29 22:44:10 -0400

    Update __init__.py in metabolism subpackage

--- microbial_ai/metabolism/__init__.py
+++ microbial_ai/metabolism/__init__.py
@@ -1,1 +1,2 @@
-from microbial_ai.metabolism import Metabolites
+from microbial_ai.metabolism.metabolite import Metabolite, ExMetabolite
+from microbial_ai.metabolism.reaction import Reaction, ExReaction
